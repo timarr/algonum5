2fdbc4c63cf20856f4e0e4ae6c29a0fa7bd37b8f
LucBoissieras <lucboissieras@gmail.com>
2017-05-04 18:29:07 +0200

    first part of the third exo done!

--- test.py
+++ test.py
@@ -3,39 +3,54 @@ import re #regexp
 import matplotlib.pyplot as plt
 import load_foil as ld
 import interpolation as inter
+import curves
 
 def display(ex, ey, ix, iy):
     plt.plot(ex, ey, linewidth = 1.0)
     plt.plot(ix, iy, linewidth = 1.0)
     plt.show()
 
+def plot_funct(x, f_x, n):
+    x_t = np.empty(0)
+    for i in range(0, x.size - 1):
+        x_tmp = np.arange(x[i], x[i + 1], (x[i + 1] - x[i])/n)
+        for j in range(0, n):
+            x_t = np.append(x_t, x_tmp[j])
+
+    y = np.empty(x_t.size)
+    for i in range(0, x_t.size):
+        y[i] = f_x(x_t[i])
+
+    plt.plot(x_t, y) 
+    
+
 def display_interpole(ex, f_ex, ix, f_ix, n):
-    ex_t = np.empty(0)
-    ix_t = np.empty(0)
-    for i in range(0, ex.size - 1):
-        ex_tmp = np.arange(ex[i], ex[i + 1], (ex[i + 1] - ex[i])/n)
-        for j in range(0, n):
-            ex_t = np.append(ex_t, ex_tmp[j])
-
-    for i in range(0, ix.size - 1):
-        ix_tmp = np.arange(ix[i], ix[i + 1], (ix[i + 1] - ix[i])/n)
-        for j in range(0, n):
-            ix_t = np.append(ix_t, ix_tmp[j])
-
-    ey_t = np.empty(ex_t.size)
-    iy_t = np.empty(ix_t.size)
-    for i in range(0, ex_t.size):
-        ey_t[i] = f_ex(ex_t[i])
-        
-    for i in range(0, ix_t.size):
-        iy_t[i] = f_ix(ix_t[i])
-
-    plt.plot(ex_t, ey_t)
-    plt.plot(ix_t, iy_t)
+    plot_funct(ex, f_ex, n)
+    plot_funct(ix, f_ix, n)
     plt.show()
 
+
+
+def display_slices(ex, f_lambdas, ix, g_lambdas, n):
+    for f in f_lambdas:
+        plot_funct(ex, f, n)
+        
+    for g in g_lambdas:
+        plot_funct(ix, g, n)
+    plt.show()
+
+
 (dim, ex,ey,ix,iy) = ld.load_foil("airfoils/b29root.dat")
-display(ex, ey, ix, iy)
+#display(ex, ey, ix, iy)
+
+number_points = 10
 f = inter.interpolation(ex, ey)
 g = inter.interpolation(ix, iy)
-display_interpole(ex, f, ix, g, 10)
+#display_interpole(ex, f, ix, g, number_points)
+
+n_curves = 10
+h_max_e = curves.find_farrest_point(ey)
+h_max_i = curves.find_farrest_point(iy)
+f_lambdas = curves.create_curves(f, n_curves, h_max_e)
+g_lambdas = curves.create_curves(g, n_curves, h_max_i)
+display_slices(ex, f_lambdas, ix, g_lambdas, number_points)
